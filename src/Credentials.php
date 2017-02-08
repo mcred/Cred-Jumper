3f82b764af807f5ae4b66bf4097db37c3bdb0ece
Derek Smart <derek@grindaga.com>
2017-02-08 12:45:46 -0500

    base Credential class and test
    Signed-off-by: Derek Smart <derek@grindaga.com>

---
+++ src/Credentials.php
@@ -0,0 +1,14 @@
+<?php
+namespace CredJumper;
+
+/**
+ * The Credentials Objects and Methods
+ *
+ * @author     Derek Smart <derek@grindaga.com>
+ */
+class Credentials
+{
+    public function __construct()
+    {
+    }
+}
---
+++ tests/unit/CredentialsTest.php
@@ -0,0 +1,9 @@
+<?php
+namespace CredJumper;
+
+/**
+* @covers \CredJumper\Credentials
+*/
+class CredentialsTest extends \PHPUnit\Framework\TestCase
+{
+}
